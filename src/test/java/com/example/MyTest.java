6c497260569b3cde120b982c462553e900d55d63
Adi-9703 <adityapandey9703@gmail.com>
2025-04-25 00:28:48 +0530

    Initial commit of Maven project

---
+++ src/test/java/com/example/MyTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class MyTest {
+
+    @Test
+    public void testMethod() {
+        System.out.println("TestNG is working!");
+    }
+}
